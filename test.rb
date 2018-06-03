721a2ce2fde638e7d9330b30010c257159c72fca
cbc106013 <34334167+cbc106013@users.noreply.github.com>
2018-06-03 10:45:03 +0800

    Add files via upload

--- test.rb
+++ test.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/ruby
 require 'watir'
-
+tstart = Time.now
+download_bool = true
 def send_msg(browser, msg)
     browser.send_keys :enter
     browser.send_keys msg
@@ -42,7 +43,7 @@ until last_chat.span.present? && last_chat.span.text == "over" do
             f = Dir.entries(Dir.pwd).reject{|f|File.ftype(f)!='file'}.sort_by{|f| File.mtime(f)}.last
             File.rename(f, "#{$user}.jpg") if Time.now-File.mtime(f)<3
             $get = last_chat.attribute_value("data-local-id")
-            
+
         end
     end
 end
@@ -63,23 +64,30 @@ if cmd = STDIN.gets
     # end
 end
 
+loop do
+  tnow = Time.now
+  if tnow - tstart > 100
+    download_bool = true
+    tstart = Time.now
+# read command from standard input:
+  if download_bool = true
+      last_chat=browser.divs(css: ".MdRGT07Cont").last
+      if last_chat.attribute_value("data-local-id")!= $get
+          if (last_image=last_chat.div(css:".mdRGT07Body .mdRGT07Msg.mdRGT07Image")).present?
+              if last_chat.div(css: ".mdRGT07Ttl").present?
+                  $user = last_chat.div(css: ".mdRGT07Ttl").text
+              else
+                  $user = '我'
+              end
+              browser.buttons(id:"_chat_message_image_save").last.click
+              sleep(1)
+              f = Dir.entries(Dir.pwd).reject{|f|File.ftype(f)!='file'}.sort_by{|f| File.mtime(f)}.last
+              File.rename(f, "#{$user}.jpg") if Time.now-File.mtime(f)<3
+              $get = last_chat.attribute_value("data-local-id")
 
-until last_chat.span.present? && last_chat.span.text == "end" do
-    last_chat=browser.divs(css: ".MdRGT07Cont").last
-    if last_chat.attribute_value("data-local-id")!= $get
-        if (last_image=last_chat.div(css:".mdRGT07Body .mdRGT07Msg.mdRGT07Image")).present?
-            if last_chat.div(css: ".mdRGT07Ttl").present?
-                $user = last_chat.div(css: ".mdRGT07Ttl").text
-            else
-                $user = '我'
-            end
-            browser.buttons(id:"_chat_message_image_save").last.click
-            sleep(1)
-            f = Dir.entries(Dir.pwd).reject{|f|File.ftype(f)!='file'}.sort_by{|f| File.mtime(f)}.last
-            File.rename(f, "#{$user}.jpg") if Time.now-File.mtime(f)<3
-            $get = last_chat.attribute_value("data-local-id")
-            
-        end
+          end
+      end
+      download_bool = false
     end
     if cmd = STDIN.gets
         # remove whitespaces:
@@ -97,21 +105,3 @@ until last_chat.span.present? && last_chat.span.text == "end" do
         end
     end
 end
-end
-
-if cmd = STDIN.gets
-    # remove whitespaces:
-    cmd.chop!
-    # if command is "exit", terminate:
-    # if cmd == "exit"
-    #     break
-    # else
-    # else evaluate command, send result to standard output:
-    print send_msg(browser, cmd),"\n"
-    # and append [end] so that master knows it's the last line:
-    print "[end]\n"
-    # flush stdout to avoid buffering issues:
-    STDOUT.flush
-    # end
-end
-
